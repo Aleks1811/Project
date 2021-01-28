f96de858e47542f6f2769d348e5d9f45bd14f3f0
Aleks1811 <Aleksmladenov1811@gmail.com>
2021-01-28 10:14:11 +0200

    PROJECT
    project_main1

---
+++ PROJECT_main/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<div>
+	   <form action="project_var(c)8.php" method="POST" class="form">
+		<input type="text" name="question" placeholder="Enter a question......" class="question_box"><br>
+		<input type="submit" name="submit" value="correct" class="btn">
+		
+	   </form>
+    </div>
+	
+</body>
+</html>
---
+++ PROJECT_main/project.php
@@ -0,0 +1,41 @@
+<?php
+
+$question = $_POST['question'];
+
+//ВАЖНО РАБОТИ И КАТО ЕДНО!
+function correcting_question($input){
+   // $comma = ",";
+   // $interval = ' ';
+   //$input = str_replace($comma, "," . $interval, $input);
+    
+     //Correcting spacing around punctuation
+     $comma_interval = ',' . ' ';
+
+     $input = preg_replace('/\s*,\s*/', $comma_interval, $input);
+
+     //Correcting whitespace
+     $input = preg_replace('/\s+/', ' ', $input);
+
+     //Correcting question mark
+     $input = preg_replace('/\?+/', '?', $input);
+
+      $mark = '?';
+      //$mark = strlen($input) -1; 
+        if(!function_exists($mark)) {
+          return ucfirst(trim($input)) . $mark;
+        }
+        
+        return ucfirst(trim($input));
+}
+echo "<pre>";
+echo "Submitted question:" . $question;
+echo "<br>";
+echo "Corrected question:" . correcting_question($question);
+echo "</pre>";
+
+// else{
+//           return ucfirst(trim($input)) . str_replace($mark, '', $input);
+//        }
+// else{
+       //  preg_replace($mark, $input, $input);
+       // }
